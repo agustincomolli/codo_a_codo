73409d837057e16d58565253cafa5ce0f1d47c52
Agustín Comolli <agustincomolli@yahoo.com.ar>
2023-09-29 15:51:32 -0300

    Agregado ejercicio de js 11

--- JS/actividad-practica-1/ejercicio-11/index.js
+++ JS/actividad-practica-1/ejercicio-11/index.js
@@ -1,1 +1,13 @@
-alert("¡Hola mundo!")
+const VOWELS = ["a", "e", "i", "o", "u"];
+let phrase = prompt("Escriba una frase:");
+let count = 0;
+
+for (let char of phrase) {
+    if (VOWELS.includes(char)) {
+        count++;
+    };
+};
+
+document.write("<p>Su frase:</p>");
+document.write("<p><em>'" + phrase + "'</em></p>");
+document.write("<p>Tiene "+ count + " vocales</p>");
