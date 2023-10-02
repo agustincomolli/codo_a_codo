122af5446964abd6b77cc1134beeed087182fdc8
Agustín Comolli <agustincomolli@gmail.com>
2023-10-02 13:49:54 -0300

    Agregada teoría Unidad 3 de JavaScript

--- JS/actividad-practica-1/ejercicio-12/index.js
+++ JS/actividad-practica-1/ejercicio-12/index.js
@@ -1,1 +1,18 @@
-alert("¡Hola mundo!")
+let phrase = prompt("Escriba una frase:");
+let total_vowels = {
+    "a": 0,
+    "e": 0,
+    "i": 0,
+    "o": 0,
+    "u": 0,
+};
+
+for (let char of phrase) {
+    if ("aeiou".includes(char.toLowerCase())) {
+        total_vowels[char]++;
+    };
+};
+
+for (let char in "aeiou") {
+    document.write("<p>" + char + ": " + total_vowels[char] + "</p>");
+};
