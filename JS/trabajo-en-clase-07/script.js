b5a233d30a37c1e78e89dbfad5d71caa189a50e6
Agustín Comolli <agustincomolli@gmail.com>
2023-10-23 20:53:11 -0300

    feat(web): finish trabajo-en-clase-07

--- JS/trabajo-en-clase-07/script.js
+++ JS/trabajo-en-clase-07/script.js
@@ -1,3 +1,29 @@
+const formSearch = document.querySelector("#search");
+
+function getProvinces() {
+    return fetch(`https://apis.datos.gob.ar/georef/api/provincias`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
+
+
+async function fillProvincesSelector() {
+    const provincesSelector = document.querySelector("#provinces");
+    const infoProvinces = await getProvinces();
+
+    if (infoProvinces.provincias.length > 0) {
+        let provinces = infoProvinces.provincias;
+        provinces.sort((a, b) => a.nombre.localeCompare(b.nombre));
+        for (let province of provinces) {
+            const optionProvince = document.createElement("option");
+            optionProvince.text = province.nombre;
+            provincesSelector.appendChild(optionProvince);
+        };
+
+    };
+};
+
+
 /**
  * Obtiene información sobre una provincia a través de una solicitud a la API de georef.
  * @param {string} province - El nombre de la provincia que se desea buscar.
@@ -23,13 +49,17 @@ async function searchProvince(event) {
     // Seleccionar el elemento de mensaje en el HTML.
     const message = document.querySelector(".message");
 
+    // Deshabilitar el botón y mostrar el estado de trabajo.
+    const btnSearch = document.querySelector("#btn-search");
+    btnSearch.toggleAttribute("disabled");
+    btnSearch.setAttribute("aria-busy", "true");
+
     // Realizar una solicitud asincrónica para obtener información sobre la provincia.
     const infoProvince = await getProvince(province);
 
     // Validar si el campo de provincia está vacío.
     if (!province) {
         message.innerHTML = "<p>Debe ingresar una provincia</p>";
-        return;
     } else {
         // Verificar si se encontraron provincias coincidentes en la respuesta.
         if (infoProvince.provincias.length > 0) {
@@ -38,6 +68,15 @@ async function searchProvince(event) {
             for (let provinceFound of provinces) {
                 message.innerHTML += `<p>${provinceFound.nombre}</p>`;
             };
+        } else {
+            message.innerHTML = "<p>No se encontró ninguna provincia</p>"
         };
     };
+    // Habilitar el botón y quitar el estado de trabajo.
+    btnSearch.toggleAttribute("disabled");
+    btnSearch.removeAttribute("aria-busy");
 }
+
+
+formSearch.addEventListener("submit", searchProvince);
+fillProvincesSelector();
