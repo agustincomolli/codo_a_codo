fed55029de80200a1f674511412bb13915184f29
Agustín Comolli <agustincomolli@gmail.com>
2023-10-25 13:48:48 -0300

    feat(web): add some stuff tu ejercicio-1 of Vue.js

--- VUE/ejercicio-1/script.js
+++ VUE/ejercicio-1/script.js
@@ -1,12 +1,74 @@
 const { createApp, ref } = Vue
 
-  createApp({
+createApp({
 
-    data() {
-        return {
-            title: "¡Bienvenido a Vue.js!",
-            subtitle: "Lo mejor está por venir",
-        }
-    },
-    
-  }).mount('#app')
+  data() {
+    return {
+      title: "¡Bienvenido a Vue.js!",
+      subtitle: "Lo mejor está por venir",
+    }
+  },
+
+}).mount('#header')
+
+
+createApp({
+
+  data() {
+    return {
+      teal_paragraph: "<p class='teal-paragraph'>Este párrafo se generó con la directiva v-hmtl.</p>",
+      url: "https://vuejs.org/",
+      url_class: "vue-url",
+      login: true,
+    }
+  },
+
+}).mount("#main")
+
+
+createApp({
+
+  data() {
+    return {
+      developer: "Evan You",
+      year: "2014",
+    }
+  },
+
+}).mount("#footer")
+
+
+createApp({
+
+  data() {
+    return {
+      name: "Agustín",
+      lastName: "Comolli",
+      age: 46
+    }
+  },
+
+  methods: {
+    details() {
+      return `${this.name} ${this.lastName} tiene ${this.age} años.`
+    }
+  },
+
+}).mount("#my-card")
+
+
+const CustomComponentTitle = {
+  template: "<h2>{{ title }}</h2>",
+  data() {
+    return {
+      title: "Mi primer componente",
+    }
+  },
+};
+
+
+const vueComponent = Vue.createApp({
+  components: {
+    "my-title": CustomComponentTitle,
+  }
+}).mount("#vue-component");
