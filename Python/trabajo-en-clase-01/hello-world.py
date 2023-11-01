ccff894df0074522549404b5dbbf002b32c157c9
Agustín Comolli <agustincomolli@gmail.com>
2023-11-01 20:31:45 -0300

    feat(back): add work in class 2

--- Python/trabajo-en-clase-01/hello-world.py
+++ Python/trabajo-en-clase-01/hello-world.py
@@ -1,4 +1,6 @@
-# Esto es un comentario en Python
+# Esto es un comentario en Python.
+
+# Con print mostramos un mensaje en la consola.
 print("¡Hola mundo!")
 
 
@@ -12,10 +14,13 @@ print(
 
 # Tipos de datos
 data_types = """
-Enteros     int         3
-Reales      float       14.7
-cadenas     string      "hola"
-lógicos     bool        true
+Enteros         int         3
+Reales          float       14.7
+cadenas         string      "hola"
+lógicos         bool        true
+lista           list        [1, "dos", 3.5]
+tupla           tuple       (1,2)
+diccionario     dict        {"nombre": "Agustín"}
 """
 print(f"\nTipos de datos:{data_types}")
 
@@ -50,6 +55,42 @@ distinto            !=
 """
 print(f"Operadores de comparación: {comparison_operators}")
 
-# Pedir datos al usuario.
+# Con input pedimos datos al usuario.
 your_name = input("Ingrese su nombre: ")
 print(f"¡Hola {your_name}!")
+
+
+# Estructuras de control.
+your_age = int(input("\nIngrese su edad: "))
+if your_age >= 18 and your_age < 65:
+    print("Eres mayor de edad")
+elif your_age >= 65:
+    print("Eres un anciano")
+else:
+    print("Eres un niño")
+
+
+# Ciclo de repetición while.
+counter = 1
+print("\nContando hasta 10 con while:")
+
+while counter < 10:
+    print(counter, end=", ")
+    counter += 1
+
+print(counter)
+
+# Ciclo de repetición for.
+print("\nContando hasta 10 con for:")
+
+for counter in range(1, 10):
+    print(counter, end=", ")
+
+print(counter)
+
+# Recorriendo una lista.
+student_califications = [4, 6, 6, 10, 7, 10, 9]
+results = ", "
+print("\nCalificaciones del estudiante:")
+for calification in student_califications:
+    print(str(calification).zfill(2))  #zfill(2) rellena con 0 para que la salida tenga 2 caractres.
